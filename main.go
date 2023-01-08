fc32edaa7f6306997dc672705d96082a9d9e0c79
Maina Wycliffe <wmmaina7@gmail.com>
2023-01-08 22:22:43 +0300

    Refactor Code For Simplicity

--- main.go
+++ main.go
@@ -6,16 +6,9 @@ import (
 	"github.com/aws/jsii-runtime-go"
 )
 
-type GolangAwsCdkDemoStackProps struct {
-	awscdk.StackProps
-}
-
-func NewGolangAwsCdkDemoStack(scope constructs.Construct, id string, props *GolangAwsCdkDemoStackProps) awscdk.Stack {
-	var sprops awscdk.StackProps
-	if props != nil {
-		sprops = props.StackProps
-	}
-	stack := awscdk.NewStack(scope, &id, &sprops)
+// NewGolangAwsCdkDemoStack makes it easy to test the stack
+func NewGolangAwsCdkDemoStack(scope constructs.Construct, id string, props *awscdk.StackProps) awscdk.Stack {
+	stack := awscdk.NewStack(scope, jsii.String(id), props)
 
 	dynamodbTable := MembersDynamoDBTable(stack)
 
@@ -31,38 +24,7 @@ func main() {
 
 	app := awscdk.NewApp(nil)
 
-	NewGolangAwsCdkDemoStack(app, "GolangAwsCdkDemoStack", &GolangAwsCdkDemoStackProps{
-		awscdk.StackProps{
-			Env: env(),
-		},
-	})
+	NewGolangAwsCdkDemoStack(app, "GolangAwsCdkDemoStack", &awscdk.StackProps{})
 
 	app.Synth(nil)
-}
-
-// env determines the AWS environment (account+region) in which our stack is to
-// be deployed. For more information see: https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-func env() *awscdk.Environment {
-	// If unspecified, this stack will be "environment-agnostic".
-	// Account/Region-dependent features and context lookups will not work, but a
-	// single synthesized template can be deployed anywhere.
-	//---------------------------------------------------------------------------
-	return nil
-
-	// Uncomment if you know exactly what account and region you want to deploy
-	// the stack to. This is the recommendation for production stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String("123456789012"),
-	//  Region:  jsii.String("us-east-1"),
-	// }
-
-	// Uncomment to specialize this stack for the AWS Account and Region that are
-	// implied by the current CLI configuration. This is recommended for dev
-	// stacks.
-	//---------------------------------------------------------------------------
-	// return &awscdk.Environment{
-	//  Account: jsii.String(os.Getenv("CDK_DEFAULT_ACCOUNT")),
-	//  Region:  jsii.String(os.Getenv("CDK_DEFAULT_REGION")),
-	// }
 }
